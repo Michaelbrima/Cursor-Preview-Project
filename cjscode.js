db2b65b3a158d67d1d7035029eee5ff8a41c7601
Michaelbrima <157252258+Michaelbrima@users.noreply.github.com>
2024-02-03 17:40:47 -0500

    Add files via upload
    Added Files.
    Please open 'cindex.html' and 'cjscode.js' and run with 'cindex.html'. 
    First update on project as code in files currently only allows the color of the background of text 'Some Text for Testing' to become green when hovered over.

---
+++ cjscode.js
@@ -0,0 +1,20 @@
+function newPreview() {
+
+var image = document.getElementById("p1").style.backgroundColor = "green";
+
+
+}
+
+function newPreviewOff() {
+
+var image = document.getElementById("p1").style.backgroundColor = "";
+
+//<a href="C:\Users\Chris\OneDrive\Documents\Michael Brima - condensed Resume.docx"> Link 1 (Resume - Word)</a>
+//<br>
+//<br>
+//<a href="C:\Users\Chris\OneDrive\Documents\Michael Brima - Proof of Address.docx">Link 2 (Proof of Address - Word)</a>
+//<br>
+//<br>
+//<a href=""C:\Users\Chris\OneDrive\Documents\Michael Brima - Fitchburg State University Fall 2023 Final Grades.pdf">Link 3 Fall 2023 Final Grades - PDF) </a>
+
+}
